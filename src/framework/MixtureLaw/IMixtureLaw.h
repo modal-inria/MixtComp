81ccd3fc179bd46b541aa99d689d14cfbb72f089
parmeet <parmeet@767a013e-c99e-49d0-9f16-a1cc993d584d>
2013-02-07 16:45:59 +0000

    some directory structural  changes
    git-svn-id: https://scm.gforge.inria.fr/svn/mixtcomp/trunk@13 767a013e-c99e-49d0-9f16-a1cc993d584d

--- src/framework/MixtureLaw/IMixtureLaw.h
+++ src/framework/MixtureLaw/IMixtureLaw.h
@@ -3,20 +3,26 @@
 /**@file IMixtureLaw.h
  * @brief
  */
+
+#include "models/Model.h"
 class IMixtureLaw
 {
   public:
     IMixtureLaw();
     virtual void InitializeStep() = 0;
-    virtual void ImputationStep() {};
-    virtual void GibbsSampling() = 0;
-    virtual void UpdateParameters(double** ) = 0;
+    virtual void ImputeStep() {};
+    virtual void SamplingStep() = 0;
+    virtual void ParamUpdateStep() = 0;
     virtual void FinalizeStep() {};
     virtual double GetProbabilty(int sample_num,int Cluster_num) = 0;
     virtual double** GetAllProbabilties();
     virtual double GetLikelihood() const = 0;
     virtual int GetFreeParameters() const = 0;
+    double* Proportions();
+    double** ClassLabels();
     virtual ~IMixtureLaw();
+  protected:
+    Model * p_Model_;
 };
 
 #endif /* IMIXTURELAW_H_ */
---
+++ src/framework/util/util.h
@@ -0,0 +1,7 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+/**@file UTIL.h
+* @brief 
+*/
+
+#endif /* UTIL_H_ */
