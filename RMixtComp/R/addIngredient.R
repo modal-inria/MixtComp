bc2f0798533e54f90e8ef1e1ccbe9ad548b912f0
elvinz <elvinz@767a013e-c99e-49d0-9f16-a1cc993d584d>
2013-11-26 17:42:02 +0000

    RMixtComp, nrow check on addIngredient
    git-svn-id: https://scm.gforge.inria.fr/svn/mixtcomp/trunk@210 767a013e-c99e-49d0-9f16-a1cc993d584d

--- RMixtComp/R/addIngredient.R
+++ RMixtComp/R/addIngredient.R
@@ -1,4 +1,19 @@
 addIngredient <- function (lm, data, model){
-  lm[length(lm)+1] <- new("MixtureModel", as.matrix(data), model, typeof(data))
+  data = as.matrix(data)
+  if (length(lm) == 0)
+  {
+    lm[1] <- new("MixtureModel", data, model, typeof(data))
+  }
+  else
+  {
+    if (nrow(lm[[1]]@data) == nrow(data))
+    {
+      lm[length(lm)+1] <- new("MixtureModel", data, model, typeof(data))
+    }
+    else
+    {
+      message("New ingredient does not have the same number of elements as other ingredient in current mixture. It has not been added.")
+    }
+  }
   return(lm)
 }
