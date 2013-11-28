a2c72ae1882333707c86be8bc56d6840a8e61045
elvinz <elvinz@767a013e-c99e-49d0-9f16-a1cc993d584d>
2013-11-28 13:47:58 +0000

    DummyCluster created to test external model implementation in RMixtComp
    git-svn-id: https://scm.gforge.inria.fr/svn/mixtcomp/trunk@219 767a013e-c99e-49d0-9f16-a1cc993d584d

--- RMixtComp/src/mixtCompCluster.cpp
+++ RMixtComp/src/mixtCompCluster.cpp
@@ -101,3 +101,25 @@ void mixtCompCluster(Rcpp::List rList, Rcpp::S4 mcClusters, int nbClusters)
       proba(iR, kR) = composer.p_tik()->elt(iS, kS);
   mcResults.slot("proba") = proba;
 }
+
+// [[Rcpp::export]]
+void dummyMixtCompCluster(Rcpp::List rList, Rcpp::S4 mcClusters, int nbClusters)
+{
+  // parse the S4 argument into input and output
+  Rcpp::S4 mcStrategy = mcClusters.slot("strategy");
+  Rcpp::S4 mcResults = mcClusters.slot("results");
+  
+  // map the data structure
+  mixt::DataHandlerR handler;
+  handler.readDataFromRList(rList);
+  handler.writeInfo(std::cout);
+  handler.writeDataMap();
+  
+  // prepare the composer
+  STK::MixtureComposer composer(nbClusters);
+  STK::IMixtureModelBase* p_composer = &composer;
+  composer.setDataHandler(&handler);
+  composer.createIngredients(); // add non stkpp models
+  composer.setData();
+  composer.initializeModel();
+}
