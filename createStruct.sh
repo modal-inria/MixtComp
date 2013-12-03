abe1289fb9d4dcc4a94fca0e46fd98bed8f37ea0
Vincent KUBICKI <vincent.kubicki@inria.fr>
2013-12-03 15:51:39 +0100

    createStruct script added with instructions; .gitignore added; symbolic links untracked

---
+++ createStruct.sh
@@ -0,0 +1,8 @@
+mkdir MixtComp/bin
+mkdir MixtComp/bin/debug
+mkdir MixtComp/bin/release
+
+ln -s $STKPP_PATH/bin RMixtComp/src/stkpp/bin
+ln -s $STKPP_PATH/include RMixtComp/src/stkpp/include
+ln -s $STKPP_PATH/lib RMixtComp/src/stkpp/lib
+ln -s $STKPP_PATH/projects RMixtComp/src/stkpp/projects
