d68b19d2a7775e032ccec4afb0e66648bc369aa2
Vincent KUBICKI <vincent.kubicki@inria.fr>
2014-01-17 18:23:06 +0100

    basic support for zip files

---
+++ RMixtComp/R/getDataZ.R
@@ -0,0 +1,35 @@
+getDataZ <- function(...){
+  argList <- list(...)
+  lm <- list()
+  
+  for (k in 1:length(argList))
+  {
+    dataFile <- argList[[k]][1]
+    descriptorFile <- argList[[k]][2]
+    data <- read.table(file = gzfile(dataFile),
+                       header = TRUE,
+                       sep = ";",
+                       quote = "\"",
+                       dec = ".",
+                       fill = FALSE,
+                       comment.char = "",
+                       stringsAsFactors = FALSE)
+    descriptors <- read.table(file = gzfile(descriptorFile),
+                              header = FALSE,
+                              sep = ";",
+                              quote = "\"",
+                              dec = ".",
+                              fill = FALSE,
+                              comment.char = "",
+                              stringsAsFactors = FALSE)
+    for (i in 1:length(data))
+    {
+      lm <- addVariable(lm,
+                        data[[i]],
+                        descriptors[1, i],
+                        descriptors[2, i]) # data is sent as a dataframe
+    }
+  }
+  
+  return(lm)
+}
--- RMixtComp/R/tests.R
+++ RMixtComp/R/tests.R
@@ -1,6 +1,15 @@
-testRMixtComp <- function (){
+testRMC <- function (){
   lm <- getData(c("data/composite1.csv", "data/descriptor1.csv"),
                 c("data/composite2.csv", "data/descriptor2.csv"))
+  
+  mcCluster <- getMixtCompCluster(2, 20, 100)
+  mixtCompCluster(lm, mcCluster, 3)
+  return(mcCluster)
+}
+
+testRMCZ <- function (){
+  lm <- getDataZ(c("data/composite1.csv.gz", "data/descriptor1.csv.gz"),
+                c("data/composite2.csv.gz", "data/descriptor2.csv.gz"))
   
   mcCluster <- getMixtCompCluster(2, 20, 100)
   mixtCompCluster(lm, mcCluster, 3)
