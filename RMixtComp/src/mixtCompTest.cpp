fe7a7e960a3d8da022fba89e5f572be22c5257bd
Vincent KUBICKI <vincent.kubicki@gmail.com>
2014-11-21 16:40:23 +0100

    mixtCompTest added as a sandbox

---
+++ RMixtComp/src/mixtCompTest.cpp
@@ -0,0 +1,34 @@
+/*--------------------------------------------------------------------*/
+/*  Copyright (C) Inria 2014
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ *  Project:    MixtComp
+ *  Created on: Nov 21, 2014
+ *  Author:     Vincent KUBICKI <vincent.kubicki@inria.fr>
+ **/
+
+#include <boost/math/distributions/normal.hpp>
+#include <iostream>
+
+// [[Rcpp::export]]
+void mixtCompTest()
+{
+  boost::math::normal s(0.2, 1.3);
+  std::cout << cdf(s, -0.32) << std::endl;
+  std::cout << cdf(s, 0.2) << std::endl;
+  std::cout << cdf(s, 12) << std::endl;
+}
