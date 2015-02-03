90d71eb17f7fec1080bc79db41e846bf4fa9ddef
Vincent KUBICKI <vincent.kubicki@gmail.com>
2015-02-03 17:30:52 +0100

    Copyright notice update

--- MixtComp/src/Composer/mixt_IMixtureComposerBase.cpp
+++ MixtComp/src/Composer/mixt_IMixtureComposerBase.cpp
@@ -1,36 +1,26 @@
 /*--------------------------------------------------------------------*/
-/*     Copyright (C) 2004-2012  Serge Iovleff
+/*  Copyright (C) Inria 2014
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as
-    published by the Free Software Foundation; either version 2 of the
-    License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+    GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this program; if not, write to the
-    Free Software Foundation, Inc.,
-    59 Temple Place,
-    Suite 330,
-    Boston, MA 02111-1307
-    USA
-
-    Contact : S..._Dot_I..._At_stkpp_Dot_org (see copyright for ...)
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
- * Project:  stkpp::Clustering
- * created on: 16 oct. 2012
- * Author:   iovleff, S..._Dot_I..._At_stkpp_Dot_org (see copyright for ...)
- * Originally created by Parmeet Bhatia <b..._DOT_p..._AT_gmail_Dot_com>
- **/
-
-/** @file STK_IMixtureComposerBase.h
- *  @brief In this file we implement the abstract base class for mixture models.
+ *  Project:    MixtComp
+ *  Created on: Nov 14, 2014
+ *  Authors:    Vincent KUBICKI <vincent.kubicki@inria.fr>
+ *              iovleff, S..._Dot_I..._At_stkpp_Dot_org (see copyright for ...)
+ *              Parmeet Bhatia <b..._DOT_p..._AT_gmail_Dot_com>
  **/
 
 #include <cmath>
