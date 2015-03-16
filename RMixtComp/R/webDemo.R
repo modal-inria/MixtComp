1bbab117aa54946332b2b456719fae8ce50813a9
Vincent KUBICKI <vincent.kubicki@gmail.com>
2015-03-16 16:50:11 +0100

    webDemo, first version

---
+++ RMixtComp/R/webDemo.R
@@ -0,0 +1,59 @@
+webDemo <- function(folderName)
+{
+  iniContent <- read.table(paste(folderName,
+                                 "in/param.ini",
+                                 sep = "/"),
+                           sep = "=",
+                           strip.white = TRUE,
+                           stringsAsFactors = FALSE)
+  
+  nbClass = iniContent[which(iniContent[, 1] == "nbCluster"), 2]
+  
+  lm <- getData(c(paste(folderName,
+                        "in/data.csv",
+                        sep = "/"),
+                  paste(folderName,
+                        "in/descriptor.csv",
+                        sep = "/")))
+  
+  # creation of strategy list
+  mcStrategy <- list(nbTrialInInit = 2,
+                     nbBurnInIter = 100,
+                     nbIter = 100,
+                     nbGibbsBurnInIter = 100,
+                     nbGibbsIter = 100)
+  
+  # launch of the MixtComp algorithm
+  res <- mixtCompCluster(lm,
+                         mcStrategy,
+                         nbClass,
+                         0.95)
+  
+  fileConn <- file(paste(folderName,
+                         "out/log.txt",
+                         sep = "/"))
+  if (nchar(res$mixture$warnLog) > 0)
+  {
+    cat(res$mixture$warnLog,
+        file = fileConn,
+        sep = "")
+  }
+  else
+  {
+    cat("Run completed successfully",
+        file = fileConn,
+        sep = "")
+  }
+  close(fileConn)
+  
+#   confMat <- confusionMatrix(paste(folderName,
+#                                    "archive/classIn.csv",
+#                                    sep = "/"),
+#                              res$variable)
+#   print(confMat)
+  
+  save(res,
+       file = paste(folderName,
+                    "out/output.RData",
+                    sep = "/"))
+}
