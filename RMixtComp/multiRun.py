3a31a29f7eec1b34057ca6db072b3670a7dda25c
Vincent KUBICKI <vincent.kubicki@inria.fr>
2015-07-23 17:22:30 +0200

    multiRun.py added to catch low probability errors more easily

---
+++ RMixtComp/multiRun.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import subprocess
+
+nbRunMax = 100
+nbFail = 0
+
+for i in range(nbRunMax):
+    print '%d / %d' % (i + 1, nbRunMax)
+    subprocess.call(["src/test/test > log 2>&1"], shell=True) # potential security flaw: https://docs.python.org/2/library/subprocess.html#frequently-used-arguments
+    line = subprocess.check_output(['tail', '-1', 'log'])
+    if line == "Aborted\n":
+        print "detected"
+        break
