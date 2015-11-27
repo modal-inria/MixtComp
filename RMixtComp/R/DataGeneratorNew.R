22068675740186627d6f87394e6b3c9f05c7ff9e
Vincent KUBICKI <vincent.kubicki@inria.fr>
2015-11-27 13:49:57 +0100

    RankGenerator, functionnal

--- RMixtComp/R/DataGeneratorNew.R
+++ RMixtComp/R/DataGeneratorNew.R
@@ -116,11 +116,45 @@ dataGeneratorNew <- function(prefix,
               descriptor = descMat))
 }
 
-rankGenerator <- function(param) {
-  muStr <- paste0(param$mu, collapse = ", ")
-  dataStr <- paste0("Data generated using mu: ", muStr,
-                    ", pi: ", param$pi)
-  return(dataStr)
+rankGenerator <- function(missing,
+                          param) {
+  nbPos <- length(param$mu)
+  muRanking <-  switchRepresentation(param$mu) # mu in ranking representation instead of ordering representation
+  y <- sample(nbPos)
+  x <- list(y[1])
+  
+  for (p in 2:nbPos) { # current element in the presentation order, or current size of the x vector + 1
+    currY <- y[p]
+    yPlaced <- FALSE # has currY been placed correctly ?
+    
+    for (i in 1:(p - 1)) {
+      comparison <- muRanking[currY] < muRanking[x[[i]]]
+      
+      if (param$pi < runif(1)) { # is the comparison incorrect ?
+        comparison <- !comparison
+      }
+      
+      if (comparison) { # currY must be placed before the current p element (and become the p-th element)
+        x <- append(x,
+                    currY,
+                    i - 1)
+        yPlaced = TRUE
+        break # no need to test further position for j element
+      }
+    }
+    
+    if (!yPlaced) { # if element j has not been placed yet, it goes at the end of x
+      x <- append(x,
+                  currY,
+                  p - 1)
+    }
+  }
+  
+  xStr <- paste(x,
+                sep = "",
+                collapse = ", ")
+  
+  return(xStr)
 }
 
 switchRepresentation <- function(inRank) {
