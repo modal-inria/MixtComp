823d042be1bb46a934e93c45ab22fc3a2bfba237
Vincent KUBICKI <vincent.kubicki@inria.fr>
2016-01-13 17:51:08 +0100

    CategoricalDataStat, clearer output format and debug

--- MixtComp/src/Data/mixt_CategoricalDataStat.cpp
+++ MixtComp/src/Data/mixt_CategoricalDataStat.cpp
@@ -38,13 +38,6 @@ CategoricalDataStat::~CategoricalDataStat() {};
 void CategoricalDataStat::sample(int ind)
 {
   int currMod = augData_.data_(ind);
-#ifdef MC_DEBUG
-  if (currMod == 0)
-  {
-    std::cout << "CategoricalDataStat::sample, modality 0 detected" << std::endl;
-  }
-  std::cout << "currMod: " << currMod << ", stat_.size(): " << stat_.size() << std::endl;
-#endif
   stat_[currMod] += 1.;
 }
 
@@ -52,35 +45,16 @@ void CategoricalDataStat::sampleVals(int ind,
                                      int iteration,
                                      int iterationMax)
 {
-#ifdef MC_DEBUG
-  std::cout << "CategoricalDataStat::sampleVals, ind: " << ind << ", iteration: " << iteration << std::endl;
-#endif
-  if (augData_.misData_(ind).first != present_)
-  {
-    if (iteration == 0) // clear the temporary statistical object
-    {
-#ifdef MC_DEBUG
-      std::cout << "CategoricalDataStat::sampleVals, iteration 0" << std::endl;
-#endif
-      // initialize internal storage
-#ifdef MC_DEBUG
-      std::cout << "pm_augDataij_.dataRange_.max_: " << augData_.dataRange_.max_ << std::endl;
-#endif
-      stat_.resize(augData_.dataRange_.max_ + 1);
+  if (augData_.misData_(ind).first != present_) {
+    if (iteration == 0) { // clear the temporary statistical object
+      stat_.resize(augData_.dataRange_.max_ + 1); // initialize internal storage
       stat_ = 0.;
 
-      // clear output storage for current individual, a vector of <modality, proba>, ordered by decreasing probability
-      // up to a cut-off defined by confidenceLevel
-      dataStatStorage_(ind) = std::vector<std::pair<int, Real> >();
+      dataStatStorage_(ind) = std::vector<std::pair<int, Real> >(); // clear output storage for current individual, a vector of <modality, proba>, ordered by decreasing probability up to a cut-off defined by confidenceLevel
 
-      // first sampling, on each missing variables
-      sample(ind);
+      sample(ind); // first sampling, on each missing variables
     }
-    else if (iteration == iterationMax) // export the statistics to the p_dataStatStorage object
-    {
-#ifdef MC_DEBUG
-      std::cout << "CategoricalDataStat::sampleVals, iterationMax" << std::endl;
-#endif
+    else if (iteration == iterationMax) { // export the statistics to the p_dataStatStorage object
       sample(ind); // last sampling
 
       stat_ /= Real(iterationMax + 1); // from count to probabilities
@@ -88,35 +62,20 @@ void CategoricalDataStat::sampleVals(int ind,
       stat_.sortIndex(indOrder);
       Real cumProb = 0.;
 
-      for (int i = augData_.dataRange_.max_ - 1; // from the most probable modality ...
+      for (int i = augData_.dataRange_.max_; // from the most probable modality ...
            i > -1; // ... to the least probable modality
-           --i)
-      {
+           --i) {
         int currMod = indOrder[i];
         Real currProba = stat_[currMod];
         dataStatStorage_(ind).push_back(std::pair<int, Real>(currMod, currProba));
         cumProb += currProba;
-#ifdef MC_DEBUG
-        std::cout << "\ti: " << i << ", currMod: " << currMod << ", proba[currMod]: " << proba[currMod] << std::endl;
-        std::cout << "\tcumProb: " << cumProb << std::endl;
-        std::cout << "p_dataStatStorage_.elt(ind, j).back().first: " << dataStatStorage_(ind).back().first << std::endl;
-#endif
-        if (cumProb > confidenceLevel_)
-        {
+
+        if (cumProb > confidenceLevel_) {
           break;
         }
       }
     }
-#ifdef MC_DEBUG
-    for (std::vector<std::pair<int, Real> >::const_iterator itVec = dataStatStorage_(ind).begin();
-         itVec != dataStatStorage_(ind).end();
-         ++itVec)
-    {
-      std::cout << "itVec.first: " << itVec.first << ", itVec.second: " << itVec.second << std::endl;
-    }
-#endif
-    else // any other iteration: juste store the current value
-    {
+    else { // any other iteration: just store the current value
       sample(ind);
     }
   }
