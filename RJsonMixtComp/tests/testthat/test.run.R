2214211e2a9d74efaa113a84d9ab59f9219aea15
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2017-02-24 10:04:21 +0100

    add test

---
+++ RJsonMixtComp/tests/testthat/test.run.R
@@ -0,0 +1,28 @@
+context("Test fonctionnel")
+
+test_that("Learn + predict", {
+  pathToData <- system.file("extdata", "data2.csv", package = "RJsonMixtComp")
+  pathToDescriptor <- system.file("extdata", "descriptor2.csv", package = "RJsonMixtComp")
+
+  resGetData <- RJsonMixtComp:::getData(c(pathToData, pathToDescriptor)) 
+  
+  
+  resLearn <- JsonMixtCompCluster(dataList = resGetData$lm, mcStrategy = list(nbBurnInIter = 100, 
+                                                                              nbIter = 100, 
+                                                                              nbGibbsBurnInIter = 100, 
+                                                                              nbGibbsIter = 100, 
+                                                                              parameterEdgeAuthorized = TRUE),
+                                  nbClass = 1, confidenceLevel = 0.95, jsonInputFile = "datalearn.json", jsonOutputFile = "reslearn.json")
+  
+  
+  resPredict <- JsonMixtCompPredict(dataList = resGetData$lm, mcStrategy = list(nbBurnInIter = 100, 
+                                                                                nbIter = 100, 
+                                                                                nbGibbsBurnInIter = 100, 
+                                                                                nbGibbsIter = 100, 
+                                                                                parameterEdgeAuthorized = TRUE),
+                                    nbClass = 1, confidenceLevel = 0.95, jsonInputFile = "datalearn.json", jsonOutputFile = "respredict.json",
+                                    jsonMixtCompLearnFile = "reslearn.json")
+  
+  
+  file.remove(c("reslearn.json", "respredict.json", "datalearn.json"))
+})
