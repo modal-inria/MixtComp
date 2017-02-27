0e868e4e7629eb5e2bf6c53c1c3281e143cfa2ad
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2017-02-27 17:34:53 +0100

    move logToMulti function

--- RMixtComp/R/GENDATA_functionalGenerator.R
+++ RMixtComp/R/GENDATA_functionalGenerator.R
@@ -175,3 +175,13 @@ functionalGenerator <- function(param) {
   
   return(xStr)
 }
+
+logToMulti <- function(logIn) {
+  max <- max(logIn);
+  multiOut <- logIn - max
+  multiOut <- exp(multiOut)
+  sum <- sum(multiOut)
+  multiOut <- multiOut / sum;
+  
+  return(multiOut)
+}
--- RMixtComp/R/GENDATA_logToMulti.R
+++
@@ -1,9 +0,0 @@
-logToMulti <- function(logIn) {
-  max <- max(logIn);
-  multiOut <- logIn - max
-  multiOut <- exp(multiOut)
-  sum <- sum(multiOut)
-  multiOut <- multiOut / sum;
-
-  return(multiOut)
-}
