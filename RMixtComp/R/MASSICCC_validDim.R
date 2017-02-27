aaf4a838e593b92f7b1df358b5db5a8c020d9efc
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2017-02-27 17:39:34 +0100

    rename validDIm file and move generateDummy

--- RMixtComp/R/MASSICCC_validDim.R
+++ RMixtComp/R/MASSICCC_validDim.R
@@ -1,18 +1,3 @@
-# Generate a random matrix of a given size and write it in the test directory. Used to
-# test validDim
-generateDummyData <- function(fileName,
-                              nRow,
-                              nCol) {
-  mat <- matrix(rexp(nRow * nCol,
-                     rate = .1),
-                ncol = nCol)
-  
-  write.table(x = mat,
-              file = fileName,
-              sep = ";",
-              row.names = FALSE,
-              col.names = FALSE)
-}
 
 # Check if the data in fileName is smaller than the provided dimensions
 validDim <- function(fileName,
--- RMixtComp/tests/testthat/test.various.R
+++ RMixtComp/tests/testthat/test.various.R
@@ -2,6 +2,22 @@ context("Various tests")
 
 test_that("validDim", {
   
+  
+  generateDummyData <- function(fileName,
+                                nRow,
+                                nCol) {
+    mat <- matrix(rexp(nRow * nCol,
+                       rate = .1),
+                  ncol = nCol)
+    
+    write.table(x = mat,
+                file = fileName,
+                sep = ";",
+                row.names = FALSE,
+                col.names = FALSE)
+  }
+  
+  
   testFile <- "mat.csv"
   
   generateDummyData(testFile, 10, 10)
