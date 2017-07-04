ba921e8e6e33ca28f2a3373ec34d69aff8e0690d
MARBAC LOURDELLE Matthieu <matthieu.marbac-lourdelle@inria.fr>
2017-07-04 09:30:47 +0200

    visu mixtcomp

--- RMixtComp/R/MASSICCC_plotClasses.R
+++ RMixtComp/R/MASSICCC_plotClasses.R
@@ -5,29 +5,36 @@
 # Here, few types of data are allowed: continuous or categorical
 # The other types of data must be implemented
 computelogtikmixcomp <- function(output){
-  param <- output$variable$param
-  x <- output$variable$data
-  logtik <- matrix(0, length(x$z_class$completed), output$mixture$nbCluster)
-  for (k in 1:output$mixture$nbCluster)  logtik[,k] <- log(param$z_class$pi$stat[k,1])
+  # param <- output$variable$param
+  # x <- output$variable$data
+  # logtik <- matrix(0, length(x$z_class$completed), output$mixture$nbCluster)
+  # for (k in 1:output$mixture$nbCluster)  logtik[,k] <- log(param$z_class$pi$stat[k,1])
+  # 
+  # for (j in 2:length(x)){
+  #   if (output$variable$type[[j]] == "Gaussian_sjk"){
+  #     for (k in 1:ncol(logtik)){
+  #       tmp <- matrix(param[[j]]$NumericalParam$stat[,1], 2, output$mixture$nbCluster)
+  #       logtik[,k] <- logtik[,k] + dnorm(x[[j]]$completed, tmp[1,k], tmp[2,k], log=TRUE)
+  #     }
+  #   }
+  #   if (output$variable$type[[j]] == "Categorical_pjk"){
+  #     for (k in 1:ncol(logtik)){
+  #       tmp <- log(matrix(param[[j]]$NumericalParam$stat[,1], ncol=output$mixture$nbCluster))
+  #       logtik[,k] <- logtik[,k] + tmp[x[[j]]$completed, k]
+  #     }
+  #   }
+  #   
+  # }
+  # logtik <- sweep(logtik, 1, apply(logtik, 1, max), "-")
+  # logtik <- sweep(logtik, 1, log(rowSums(exp(logtik))), "-")
+  # logtik
+  sweep(output$mixture$lnProbaGivenClass, 
+        1,
+        apply(output$mixture$lnProbaGivenClass,
+              1,
+              function(vec) (max(vec) + log(sum(exp(vec - max(vec)))))),
+        "-")
   
-  for (j in 2:length(x)){
-    if (output$variable$type[[j]] == "Gaussian_sjk"){
-      for (k in 1:ncol(logtik)){
-        tmp <- matrix(param[[j]]$NumericalParam$stat[,1], 2, output$mixture$nbCluster)
-        logtik[,k] <- logtik[,k] + dnorm(x[[j]]$completed, tmp[1,k], tmp[2,k], log=TRUE)
-      }
-    }
-    if (output$variable$type[[j]] == "Categorical_pjk"){
-      for (k in 1:ncol(logtik)){
-        tmp <- log(matrix(param[[j]]$NumericalParam$stat[,1], ncol=output$mixture$nbCluster))
-        logtik[,k] <- logtik[,k] + tmp[x[[j]]$completed, k]
-      }
-    }
-    
-  }
-  logtik <- sweep(logtik, 1, apply(logtik, 1, max), "-")
-  logtik <- sweep(logtik, 1, log(rowSums(exp(logtik))), "-")
-  logtik
 }
 
 
@@ -50,7 +57,7 @@ rlogtikmixcomp <- function(output, sample.size){
 ##' @param nbcpu numeric. It specifies the number of CPU (only for linux, R package parallel must by loaded).
 ##'
 
-clusvisMixcomp <- function(resmixcomp, sample.size=5000, maxit=10**3, nbrandomInit=12, nbcpu=3){
+clusvisMixcomp <- function(resmixcomp, sample.size=5000, maxit=10**3, nbrandomInit=12, nbcpu=1){
   logtik.estim <- rlogtikmixcomp(output)
   out <- clusvis(logtik.estim, prop=resmixcomp$variable$param$z_class$pi$stat[,1], logtik.obs=computelogtikmixcomp(resmixcomp), maxit, nbrandomInit, nbcpu)
   return(out)
