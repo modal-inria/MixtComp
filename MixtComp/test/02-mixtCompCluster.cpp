aa35612db07b3eeb10889f1d44d8ceb170b4b682
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-03-21 14:21:59 +0100

    getenv test

--- MixtComp/test/02-mixtCompCluster.cpp
+++ MixtComp/test/02-mixtCompCluster.cpp
@@ -13,49 +13,42 @@
 #include "../IO/mixt_DataExtractorCsv.h"
 #include "../IO/mixt_ParamExtractorCsv.h"
 
-int main()
-{
-  Real confidenceLevel = 0.95;
-  int nbClusters = 3;
+int main() {
+	Real confidenceLevel = 0.95;
+	int nbClusters = 3;
 
-  // create the data handler
-  mixt::DataHandlerCsv handler("data/gaussian.data.csv",
-                               "data/gaussian.desc.csv");
-  handler.listData();
-  handler.writeInfo(std::cout);
-  handler.writeDataMap();
+	// create the data handler
+	mixt::DataHandlerCsv handler("data/gaussian.data.csv",
+			"data/gaussian.desc.csv");
+	handler.listData();
+	handler.writeInfo(std::cout);
+	handler.writeDataMap();
 
-  // create the data extractor
-  mixt::DataExtractorCsv dataExtractor;
+	// create the data extractor
+	mixt::DataExtractorCsv dataExtractor;
 
-  // create the parameters extractor
-  mixt::ParamExtractorCsv paramExtractor;
+	// create the parameters extractor
+	mixt::ParamExtractorCsv paramExtractor;
 
-  // create the mixture manager
-  mixt::MixtureManager<mixt::DataHandlerCsv,
-                       mixt::DataExtractorCsv,
-                       mixt::ParamExtractorCsv> manager(&handler,
-                                                        &dataExtractor,
-                                                        &paramExtractor,
-                                                        confidenceLevel);
+	// create the mixture manager
+	mixt::MixtureManager<mixt::DataHandlerCsv, mixt::DataExtractorCsv,
+			mixt::ParamExtractorCsv> manager(&handler, &dataExtractor,
+			&paramExtractor, confidenceLevel);
 
-  // prepare the composer
-  mixt::MixtureComposer composer(handler.nbSample(),
-                                 handler.nbVariable(),
-                                 nbClusters);
+	// prepare the composer
+	mixt::MixtureComposer composer(handler.nbSample(), handler.nbVariable(),
+			nbClusters);
 
-  manager.createMixtures(&composer,
-                         nbClusters);
+	manager.createMixtures(&composer, nbClusters);
 
-  // create the appropriate strategy and transmit the parameters
-  mixt::SemStrategy strategy(&composer,
-                             3, // number of trials of the complete chain
-                             114, // number of burn-in iterations
-                             507, // number of iterations
-                             123, // number of iterations for Gibbs sampler burn-in
-                             512, // number of iterations for Gibbs sampler
-                             10); // number of sampling attempts for lowly populated classes
+	// create the appropriate strategy and transmit the parameters
+	mixt::SemStrategy strategy(&composer, 3, // number of trials of the complete chain
+			114, // number of burn-in iterations
+			507, // number of iterations
+			123, // number of iterations for Gibbs sampler burn-in
+			512, // number of iterations for Gibbs sampler
+			10); // number of sampling attempts for lowly populated classes
 
-  // run the strategy
-  strategy.run();
+	// run the strategy
+	strategy.run();
 }
