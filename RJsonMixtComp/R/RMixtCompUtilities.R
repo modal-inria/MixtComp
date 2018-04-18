9a2e3754b3d203bf3d073c5033dd74df65e9da9d
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-04-18 09:15:05 +0200

    add getter in rjsonmixtcomp

--- RJsonMixtComp/R/RMixtCompUtilities.R
+++ RJsonMixtComp/R/RMixtCompUtilities.R
@@ -125,110 +125,185 @@ addVariable <- function (lm,
   return(list(lm = lm,
               warnLog = warnLog))
 }
-# 
-# 
-# # getter sortie mixtcomp --------------------------------------------------
-# 
-# completed <- function(outMixtcomp, with.z_class = FALSE)
-# {
-#   completedData <- do.call(cbind, lapply(outMixtcomp$variable$data, function(x){x$completed}))
-#   
-#   if(!with.z_class)
-#     completedData = completedData[,-which(colnames(completedData)=="z_class")]
-#   
-#   return(as.data.frame(completedData))
-# }
-# 
-# 
-# 
-# getZ_class <- function(outMixtcomp)
-# {
-#   return(outMixtcomp$variable$data$z_class$completed)
-# }
-# 
-# 
-# 
-# getType <- function(outMixtcomp, with.z_class = FALSE)
-# {
-#   type <- unlist(outMixtcomp$variable$type)
-#   
-#   if(!with.z_class)
-#     type = type[-which(names(type)=="z_class")]
-#   
-#   return(type)
-# }
-# 
-# 
-# getParam <- function(varName, outMixtComp)
-# {
-#  
-#   if(varName == "z_class")
-#     return(outMixtComp$variable$param$z_class$pi$stat$median)
-#    
-#   type <- outMixtComp$variable$type[[varName]]
-#   param <- switch(type,
-#                   "Ordinal" = outMixtComp$variable$param[[varName]]$muPi$stat$median,
-#                   "Categorical_pjk" = outMixtComp$variable$param[[varName]]$NumericalParam$stat$median,
-#                   "Gaussian_sjk" = outMixtComp$variable$param[[varName]]$NumericalParam$stat$median,
-#                   "Poisson_k" = outMixtComp$variable$param[[varName]]$NumericalParam$stat$median)
-#   
-#   return(param)
-# }
-# 
-# 
-# 
-# getParamLog <- function(varName, outMixtComp)
-# {
-#   
-#   if(varName == "z_class")
-#     return(do.call(rbind, outMixtComp$variable$param$z_class$pi$log))
-#   
-#   type <- outMixtComp$variable$type[[varName]]
-#   param <- switch(type,
-#                   "Ordinal" = do.call(rbind, outMixtComp$variable$param[[varName]]$muPi$log),
-#                   "Categorical_pjk" = do.call(rbind, outMixtComp$variable$param[[varName]]$NumericalParam$log),
-#                   "Gaussian_sjk" = do.call(rbind, outMixtComp$variable$param[[varName]]$NumericalParam$log),
-#                   "Poisson_k" = do.call(rbind, outMixtComp$variable$param[[varName]]$NumericalParam$log))
-#   
-#   return(param)
-# }
-# 
-# 
-# getParamName <- function(varName, outMixtComp)
-# {
-#   
-#   if(varName == "z_class")
-#     return("proportion")
-#   
-#   type <- outMixtComp$variable$type[[varName]]
-#   param <- switch(type,
-#                   "Categorical_pjk" = 1:as.numeric(substr(outMixtComp$variable$param[[varName]]$NumericalParam$paramStr,11,nchar(outMixtComp$variable$param[[varName]]$NumericalParam$paramStr))),
-#                   "Gaussian_sjk" = c("mean", "sd"),
-#                   "Poisson_k" = "lambda")
-#   
-#   return(param)
-# }
-# 
-# 
-# 
-# getBIC <- function(outMixtComp)
-# {
-#   if(is.null(outMixtComp$mixture$BIC))
-#     return(NaN)
-#   
-#   return(outMixtComp$mixture$BIC)
-# }
-# 
-# getICL <- function(outMixtComp)
-# {
-#   if(is.null(outMixtComp$mixture$ICL))
-#     return(NaN)
-#   
-#   return(outMixtComp$mixture$ICL)
-# }
-# 
-# 
-# getTik <- function(outMixtComp)
-# {
-#   return(outMixtComp$variable$data$z_class$stat)
-# }
+
+
+# getter sortie mixtcomp --------------------------------------------------
+#' @title Get the completed data from MixtComp object
+#'
+#' @description Get the completed data from MixtComp object
+#'
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict} functions.
+#' @param with.z_class if TRUE, z_class is returned with the data.
+#'
+#' @return  a matrix with the data completed by MixtComp (z_class is in the first column and then variables are sorted in alphabetic order, it may differ from the original order of the data).
+#'
+#' 
+#' @export
+getCompletedData <- function(outMixtComp, with.z_class = FALSE)
+{
+  completedData <- do.call(cbind, lapply(outMixtComp$variable$data, function(x){x$completed}))
+  
+  if(!with.z_class)
+    completedData = completedData[,-which(colnames(completedData)=="z_class"), drop = FALSE]
+  
+  return(as.data.frame(completedData))
+}
+
+
+
+#' @title Get the estimated class from MixtComp object
+#'
+#' @description Get the estimated class from MixtComp object
+#'
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict} functions.
+#'
+#' @return a vector containing the estimated class for each individual.
+#' 
+#' @export
+getZ_class <- function(outMixtComp)
+{
+  return(outMixtComp$variable$data$z_class$completed)
+}
+
+
+
+#' @title Get the type of model for each variable
+#'
+#' @description Get the type of model for each variable
+#'
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict} functions.
+#' @param with.z_class if TRUE, the type of z_class is returned.
+#'
+#' @return a vector containing the type of models associated with each individual.
+#'
+#' 
+#' @export
+getType <- function(outMixtComp, with.z_class = FALSE)
+{
+  type <- unlist(outMixtComp$variable$type)
+  
+  if(!with.z_class)
+    type = type[-which(names(type)=="z_class")]
+  
+  return(type)
+}
+
+
+#' @title Get the empiric tik
+#'
+#' @description Get the empiric tik
+#'
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict} functions.
+#'
+#' @return a matrix containing the tik for each individuals (in row) and each class (in column).
+#' 
+#' @export
+getTik <- function(outMixtComp)
+{
+  return(outMixtComp$variable$data$z_class$stat)
+}
+
+
+
+#' @title Get the estimated parameter
+#'
+#' @description Get the estimated parameter
+#'
+#' @param varName name of the variable to get parameter
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict}  functions.
+#'
+#' @return the parameter of the variable
+#'
+#' 
+#' @export
+getParam <- function(varName, outMixtComp)
+{
+  
+  if(varName == "z_class")
+    return(outMixtComp$variable$param$z_class$pi$stat$median)
+  
+  type <- outMixtComp$variable$type[[varName]]
+  param <- switch(type,
+                  "Ordinal" = outMixtComp$variable$param[[varName]]$muPi$stat[,1],
+                  "Categorical_pjk" = outMixtComp$variable$param[[varName]]$NumericalParam$stat[,1],
+                  "Gaussian_sjk" = outMixtComp$variable$param[[varName]]$NumericalParam$stat[,1],
+                  "Poisson_k" = outMixtComp$variable$param[[varName]]$NumericalParam$stat[,1],
+                  "Rank" = list(pi = outMixtComp$variable$param[[varName]]$pi$stat, mu = outMixtComp$variable$param[[varName]]$mu$stat),
+                  "Functional" = list(alpha = outMixtComp$variable$param[[varName]]$alpha$stat[,1],
+                                      beta = outMixtComp$variable$param[[varName]]$beta$stat[,1],
+                                      sd = outMixtComp$variable$param[[varName]]$sd$stat[,1]))
+  
+  return(param)
+}
+
+
+#' @name getBIC
+#' @aliases getICL
+#' 
+#' @usage getBIC(outMixtComp)
+#' getICL(outMixtComp)
+#' 
+#' @title Get criterion value
+#'
+#' @description Get criterion value
+#'
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict} functions.
+#'
+#' @return value of the criterion
+#'
+#' @export
+getBIC <- function(outMixtComp)
+{
+  if(is.null(outMixtComp$mixture$BIC))
+    return(NaN)
+  
+  return(outMixtComp$mixture$BIC)
+}
+
+#' @export
+getICL <- function(outMixtComp)
+{
+  if(is.null(outMixtComp$mixture$ICL))
+    return(NaN)
+  
+  return(outMixtComp$mixture$ICL)
+}
+
+
+#' @title Convert an R object to json
+#'
+#' @description convert an R object to a json file to use with \link{JsonMixtCompPredict} function
+#'
+#' @param outMixtComp output object of \link{JsonMixtCompCluster} or \link{JsonMixtCompPredict}  functions.
+#' @param filename name of the variable to get parameter
+#'
+#' @export
+writeJsonParamFile <- function(outMixtComp, filename)
+{
+  
+  # TODO ne marche qu'avec du gaussien
+  res2 <- outMixtComp
+  res2$mixture = NULL
+  res2$strategy = NULL
+  res2$variable$param$z_class$pi$log = NULL
+  res2$variable$data = NULL
+  
+  varName <- setdiff(names(outMixtComp$variable$type), "z_class")
+  
+  res2$variable$param$z_class$pi$stat <- list(median = outMixtComp$variable$param$z_class$pi$stat[,1],
+                                              "q 2.5%" = outMixtComp$variable$param$z_class$pi$stat[,2],
+                                              "q 97.5%" = outMixtComp$variable$param$z_class$pi$stat[,3])
+  
+  
+  for(nom in varName)
+  {
+    res2$variable$param[[nom]]$NumericalParam$stat <- list(median = outMixtComp$variable$param[[nom]]$NumericalParam$stat[,1],
+                                                           "q 2.5%" = outMixtComp$variable$param[[nom]]$NumericalParam$stat[,2],
+                                                           "q 97.5%" = outMixtComp$variable$param[[nom]]$NumericalParam$stat[,3])
+    
+  }
+  
+  
+  write(x = jsonlite::toJSON(res2, auto_unbox = TRUE), filename)
+  
+}
