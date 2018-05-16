83d3b825a74a2182bf3b759172457d7d09f0b068
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-05-16 17:42:44 +0200

    quantile valgrind error removed

--- MixtComp/src/lib/Various/mixt_Various.cpp
+++ MixtComp/src/lib/Various/mixt_Various.cpp
@@ -15,24 +15,20 @@
 
 namespace mixt {
 
-void writeProgress(int group,
-                   int groupMax,
-                   int iteration,
-                   int iterationMax) {
-  Real groupSize = 1. / (Real)(groupMax + 1); // group size in the progress bar
-  Real startPoint = (Real)group / (Real)(groupMax + 1);
-  Real inGroupPoint = (Real)iteration / (Real)(iterationMax + 1);
-  Real globalPoint = startPoint + inGroupPoint * groupSize;
+void writeProgress(int group, int groupMax, int iteration, int iterationMax) {
+	Real groupSize = 1. / (Real) (groupMax + 1); // group size in the progress bar
+	Real startPoint = (Real) group / (Real) (groupMax + 1);
+	Real inGroupPoint = (Real) iteration / (Real) (iterationMax + 1);
+	Real globalPoint = startPoint + inGroupPoint * groupSize;
 
-  std::ofstream myfile;
-  if(const char* env_p = std::getenv("WORKER_PROGRESS_FILE")) { // has the WORKER_PROGRESS_FILE variable been defined ?
-    myfile.open(env_p, std::ios::out | std::ios::binary);
-  }
-  else {
-    myfile.open(progressFile.c_str(), std::ios::out | std::ios::binary);
-  }
-  myfile << globalPoint;
-  myfile.close();
+	std::ofstream myfile;
+	if (const char* env_p = std::getenv("WORKER_PROGRESS_FILE")) { // has the WORKER_PROGRESS_FILE variable been defined ?
+		myfile.open(env_p, std::ios::out | std::ios::binary);
+	} else {
+		myfile.open(progressFile.c_str(), std::ios::out | std::ios::binary);
+	}
+	myfile << globalPoint;
+	myfile.close();
 }
 
 } // namespace mixt
