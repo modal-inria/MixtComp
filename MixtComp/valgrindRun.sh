41cf8299060fb2fa1dbec2787de11977e2d5cda2
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-05-17 13:39:37 +0200

    valgrindRun updated

--- MixtComp/valgrindRun.sh
+++ MixtComp/valgrindRun.sh
@@ -1,1 +1,1 @@
-valgrind --xml=yes --xml-file=valgrindReport.xml debug/utest/runUtest
+MC_DETERMINISTIC=T valgrind --xml=yes --xml-file=valgrindReport.xml debug/utest/runUtest --gtest_output=xml:uTestReport.xml
