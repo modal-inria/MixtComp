faa4de3601c75b6c2176f84cd9dcdaafba8b5df7
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-06-19 17:30:34 +0200

    export copied functions from MixtComp

--- RJsonMixtComp/R/packageDescription.R
+++ RJsonMixtComp/R/packageDescription.R
@@ -1,5 +1,16 @@
 # packages to import for the NAMESPACE file
 #' @import jsonlite
+# export functions from RMixtComp
+#' @export getBIC
+#' @export getCompletedData
+#' @export getData
+#' @export getICL
+#' @export getParam
+#' @export getTik
+#' @export getType
+#' @export getZ_class
+#' @export getVarNames
+#' 
 
 
 # functions to import for the NAMESPACE file.
