b57d4fadf54b7e33120fefe658407cd7dda71cfa
Etienne Goffinet <etienne.goffinet@inria.fr>
2018-06-26 16:04:35 +0200

    Ajout de data catégorielles dans le jeu de données généré

--- MixtcompRTools/R/generate_data.r
+++ MixtcompRTools/R/generate_data.r
@@ -1,36 +1,51 @@
-library(RMixtComp)
 
-param <- list(
-  x = c(0., 10., 20.),
-  y = c(0., 10., 0.),
-  sd = 0.1,
-  tMin = 0.,
-  tMax = 20.,
-  nTime = 100)
+generate_data <- function(size=1000) {
+  param <- list(
+    x = c(0., 10., 20.),
+    y = c(0., 10., 0.),
+    sd = 0.1,
+    tMin = 0.,
+    tMax = 20.,
+    nTime = 100
+  )
 
-data = c()
-for(i in 1:2000){
-  data <- c(data,functionalInterPolyGenerator(param=param))
+  data_func = c()
+  for (i in 1:size) {
+    data_func <- c(data_func, functionalInterPolyGenerator(param = param))
+  }
+
+  data_gaussian = rnorm(
+    n = size,
+    mean = c(0, 5, 10, 100),
+    sd = c(1, 2, 3, 4)
+  )
+
+  data_categorical = sample(1:8,size = size,replace=T)
+
+    data <- cbind(data_func,data_gaussian,data_categorical)
+  colnames(data) = c("F1", "X1", "C1")
+  descriptor <-
+    setNames(data.frame(matrix(
+      ncol = ncol(data),
+      nrow = 2,
+      byrow = TRUE
+    )), colnames(data))
+  descriptor[1, ] <-
+    c("Functional", "Gaussian_sjk", "Categorical_pjk")
+  descriptor[2, 1] <- rep("nSub: 5, nCoeff: 2", 1)
+
+  write.table(
+    data,
+    "data/generated_data.csv" ,
+    sep = ";",
+    na = "",
+    row.names = F
+  )
+  write.table(
+    descriptor,
+    file = "data/descriptor.csv",
+    sep = ";",
+    na = "",
+    row.names = F
+  )
 }
-
-data <- cbind(data,rnorm(n = 2000,mean = c(0,5,10,100),sd=c(1,2,3,4)))
-colnames(data) = c("F1","X1")
-descriptor <-
-  setNames(data.frame(matrix(
-    ncol = ncol(data),
-    nrow = 2,
-    byrow = TRUE
-  )), colnames(data))
-descriptor[1,] <-
-  c("Functional","Gaussian_sjk")
-descriptor[2, 1] <- rep("nSub: 5, nCoeff: 2", 1)
-
-write.table(
-  data,
-  "data/data_mixtcomp.csv" ,
-  sep = ";",
-  na = "",
-  row.names = F
-)
-write.table(descriptor,file="data/descriptor.csv",sep=";",na = "",row.names = F)
-
