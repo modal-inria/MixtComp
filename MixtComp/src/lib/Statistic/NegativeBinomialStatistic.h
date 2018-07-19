ff5803cbfa6d9477be2597385e338216278252a5
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-07-19 11:53:11 +0200

    add NegativeBinomialStatistic class + test

---
+++ MixtComp/src/lib/Statistic/NegativeBinomialStatistic.h
@@ -0,0 +1,45 @@
+/* MixtComp version 2.0  - 13 march 2017
+ * Copyright (C) Inria - Lille 1 */
+
+/*
+ *  Project:    MixtComp
+ *  Created on: July 19, 2018
+ *  Authors:    Quentin GRIMONPREZ <quentin.grimonprez@inria.fr>
+ **/
+
+#ifndef NEGATIVEBINOMIALSTATISTIC_H
+#define NEGATIVEBINOMIALSTATISTIC_H
+
+#include "../LinAlg/mixt_LinAlg.h"
+#include <boost/random/mersenne_twister.hpp>
+
+namespace mixt {
+
+// f(x;n,p) = Γ(x+n)/(Γ(n) x!) p^n (1-p)^x
+// with x a non negative integer
+// 0 < p < 1
+// n : number of failure (positive integer)
+
+//\f$\displaystyle P(i) = {k+i-1\choose i}p^k(1-p)^i\f$.
+class NegativeBinomialStatistic {
+public:
+	NegativeBinomialStatistic();
+
+	/** pdf evaluated at individual x */
+	Real pdf(int x, size_t n, Real p) const;
+
+	Real lpdf(int x, size_t n, Real p) const;
+
+	/** Sample a value from a Negative Binomial Law with parameter n and p */
+	int sample(size_t n, Real p);
+
+
+private:
+	/** Random number generator */
+	boost::mt19937 rng_;
+
+};
+
+} // namespace mixt
+
+#endif // NEGATIVEBINOMIALSTATISTIC_H
---
+++ MixtComp/src/utest/Statistic/UTestNegativeBinomialStatistic.cpp
@@ -0,0 +1,28 @@
+/* MixtComp version 2.0  - 13 march 2017
+ * Copyright (C) Inria - Lille 1 */
+
+/*
+ *  Project:    MixtComp
+ *  Created on: July 19, 2018
+ *  Authors:    Quentin GRIMONPREZ <quentin.grimonprez@inria.fr>
+ **/
+
+#include "../UTest.h"
+
+using namespace mixt;
+
+/**
+ * Numerically check that lpdf is the log of pdf
+ * */
+TEST(NegativeBinomialStatistic, DistributionFunctions) {
+	size_t n = 10;
+	Real p = 0.6;
+	size_t obs = 3;
+
+	NegativeBinomialStatistic nBinom;
+
+	Real lpdf0 = std::log(nBinom.pdf(obs, n, p));
+	Real lpdf1 = nBinom.lpdf(obs, n, p);
+
+	ASSERT_NEAR(lpdf0, lpdf1, epsilon);
+}
