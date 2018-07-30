b770c5b7dca78cf0ffd934d5a22b4116bff51dcb
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-07-30 14:54:29 +0200

    clean before building RMixtComp

--- RMixtComp/mcRunRTest.sh
+++ RMixtComp/mcRunRTest.sh
@@ -1,2 +1,3 @@
+make cleanRMCbase
 make all
 Rscript -e 'library(methods); library(testthat); library(RMixtComp); options(testthat.output_file = "RMCtest.xml"); test_dir("tests/testthat/", reporter = JunitReporter)'
