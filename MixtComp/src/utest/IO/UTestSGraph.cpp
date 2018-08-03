88bcdd9631c7e1163ec25f498be4a850c68dfda0
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-08-03 11:53:37 +0200

    SGraph skeleton added

---
+++ MixtComp/src/utest/IO/UTestSGraph.cpp
@@ -0,0 +1,12 @@
+/* MixtComp version 2.0  - 13 march 2017
+ * Copyright (C) Inria - Lille 1 */
+
+/*
+ *  Project:    MixtComp
+ *  Created on: August 3, 2018
+ *  Author:     Vincent KUBICKI <vincent.kubicki@inria.fr>
+ **/
+
+#include "../UTest.h"
+
+using namespace mixt;
