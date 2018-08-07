b2628fe81b73a875243ea1f3fc281a393250c07f
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-08-07 18:30:47 +0200

    Learn.cpp first draft

---
+++ MixtComp/src/lib/Run/Learn.cpp
@@ -0,0 +1,40 @@
+/* MixtComp version 2.0  - 13 march 2017
+ * Copyright (C) Inria - Lille 1 */
+
+/*
+ *  Project:    MixtComp
+ *  Created on: August 7, 2018
+ *  Authors:    Vincent KUBICKI <vincent.kubicki@inria.fr>
+ **/
+
+#include <boost/variant.hpp>
+#include <iostream>
+
+#include "Learn.h"
+#include <LinAlg/mixt_LinAlg.h>
+#include <Statistic/mixt_RNG.h>
+#include <Various/mixt_Constants.h>
+#include <Various/mixt_Timer.h>
+
+namespace mixt {
+
+/**
+ * The learn algorithm is encapsulated in a function.
+ */
+SGraph learn(const SGraph& data, const SGraph& desc, const SGraph& algo) {
+	SGraph res;
+
+	std::cout << "MixtComp, learn, version: " << version << std::endl;
+	std::cout << "Deterministic mode: " << deterministicMode() << std::endl;
+	//	std::cout<< "Number of threads: " << omp_get_num_threads() << std::endl;
+
+	Timer totalTimer("Total Run");
+
+	std::string warnLog; // string to log warnings
+
+	Real confidenceLevel = boost::get<Real>(algo.get_payload("confidenceLevel"));
+
+	return res;
+}
+
+}
---
+++ MixtComp/src/lib/Run/Learn.h
@@ -0,0 +1,24 @@
+/* MixtComp version 2.0  - 13 march 2017
+ * Copyright (C) Inria - Lille 1 */
+
+/*
+ *  Project:    MixtComp
+ *  Created on: August 7, 2018
+ *  Authors:    Vincent KUBICKI <vincent.kubicki@inria.fr>
+ **/
+
+#ifndef LEARN_H
+#define LEARN_H
+
+#include <IO/SGraph.h>
+
+namespace mixt {
+
+/**
+ * The learn algorithm is encapsulated in this function.
+ */
+SGraph learn(const SGraph& g);
+
+}
+
+#endif
