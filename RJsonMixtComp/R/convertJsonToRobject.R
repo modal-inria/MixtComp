f66df51f15b9359fa13b3dfeddd251b4b9a5b7de
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-08-24 17:28:41 +0200

    convert matrix and vector object

---
+++ RJsonMixtComp/R/convertJsonToRobject.R
@@ -0,0 +1,25 @@
+convertJsonElement <- function(x)
+{
+  if("ctype" %in% names(x))
+  {
+    x <- switch(x$ctype,
+                "Matrix" = {
+                  if((x$nrow == 0) | (x$ncol = 0))
+                    return(matrix(nrow = x$nrow, ncol = x$ncol, dimnames = list(x$rowNames, x$colNames)))
+                  
+                  rownames(x$data) = x$rowNames
+                  colnames(x$data) = x$colNames
+                  
+                  return(x$data)
+                }, 
+                "Vector" = {
+                  if(x$nrow == 0)
+                    return(c())
+                  if(length(x$rowNames) != 0) 
+                    names(x$data) = x$rowNames
+                  
+                  return(x$data)
+                })
+  }
+  return(x)
+}
