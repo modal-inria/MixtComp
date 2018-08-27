7ebfa9aba6f95c40ff95873ce5dc84fd8c5fd5bc
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-08-27 12:05:30 +0200

    update update.sh + add BinaryFiles file

--- RJsonMixtComp/update.sh
+++ RJsonMixtComp/update.sh
@@ -1,7 +1,6 @@
-cp ../RMixtComp/R/MIXTCOMP_getData.R ./R/MIXTCOMP_getData.R
 cp ../RMixtComp/R/MIXTCOMP_getter.R ./R/MIXTCOMP_getter.R
 cp ../RMixtComp/R/MIXTCOMP_misc.R ./R/MIXTCOMP_misc.R
-cd ../JsonMixtcomp/
+cd ../MixtComp/
 ./clean.sh
-./buildRelease.sh
-cp release/exec/JsonMixtComp ../RJsonMixtComp/inst/exeMixtComp/JsonMixtComp
+./buildDebug.sh
+cp debug/json/jmc ../RJsonMixtComp/inst/exeMixtComp/jmc
