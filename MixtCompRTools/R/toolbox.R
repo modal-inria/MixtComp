a3b67693df020fb745f73bfdd9e60dfb0e858295
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-10-03 16:49:18 +0200

    rename functional toolbox and add rand index and refactorCategorical

--- MixtCompRTools/R/toolbox.R
+++ MixtCompRTools/R/toolbox.R
@@ -74,3 +74,24 @@ linesFunctional <- function(x, ...)
   return(lines(newX[,1], newX[,2], type = "s", ...))
 }
 
+
+# refactor categorical data
+refactorCategoricalData <- function(data, ancienneCategorie, nouvelleCategorie)
+{
+  dataTemp <- nouvelleCategorie[match(data, ancienneCategorie)]
+
+  return(dataTemp)
+}
+
+# rand index between two partitions
+rand.index <- function (group1, group2)
+{
+  x <- abs(sapply(group1, function(x) x - group1))
+  x[x > 1] <- 1
+  y <- abs(sapply(group2, function(x) x - group2))
+  y[y > 1] <- 1
+  sg <- sum(abs(x - y))/2
+  bc <- choose(dim(x)[1], 2)
+  ri <- 1 - sg/bc
+  return(ri)
+}
