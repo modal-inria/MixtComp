993a935a43d21844d09cef9d54d5730f98f9cd5f
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-10-03 17:43:08 +0200

    update getData

--- MixtCompRTools/R/data_processing_for_mixtcomp.r
+++ MixtCompRTools/R/data_processing_for_mixtcomp.r
@@ -62,59 +62,62 @@ getData <- function(...) {
   for (k in 1:length(argList)) {
     dataFile <- argList[[k]][1]
     descriptorFile <- argList[[k]][2]
-    data <- read.table(
-      file = dataFile,
-      header = TRUE,
-      sep = ";",
-      quote = "\"",
-      dec = ".",
-      fill = FALSE,
-      comment.char = "",
-      stringsAsFactors = FALSE,
-      na.strings = ""
-    )
-    descriptor <- read.table(
-      file = descriptorFile,
-      header = FALSE,
-      sep = ";",
-      quote = "\"",
-      dec = ".",
-      fill = FALSE,
-      comment.char = "",
-      stringsAsFactors = FALSE
-    )
-    for (i in 1:length(descriptor)) {
-      # loop over columns of the descriptor
-      currId <- descriptor[1, i]
-      currModel <- descriptor[2, i]
-      if (dim(descriptor)[1] == 3) {
-        param <-
-          descriptor[3, i] # a character string describing the parameter space, that will be parsed directly by the model
+
+    if(!is.list(dataFile))
+    {
+      data <- read.table(file = dataFile,
+                         header = TRUE,
+                         sep = ";",
+                         quote = "\"",
+                         dec = ".",
+                         fill = FALSE,
+                         comment.char = "",
+                         stringsAsFactors = FALSE,
+                         na.strings = "")
+    }else{
+      data <- dataFile[[1]]
+    }
+
+
+    if(!is.list(descriptorFile))
+    {
+      descriptor <- read.table(file = descriptorFile,
+                               header = TRUE,
+                               sep = ";",
+                               quote = "\"",
+                               dec = ".",
+                               fill = FALSE,
+                               comment.char = "",
+                               stringsAsFactors = FALSE)
+    }else{
+      descriptor <- descriptorFile[[1]]
+    }
+
+    nameDesc <- colnames(descriptor)
+
+    for (i in 1:length(nameDesc)) { # loop over columns of the descriptor
+      currId <- nameDesc[i]
+      currModel <- descriptor[1, i]
+      if (dim(descriptor)[1] == 2) {
+        param <- descriptor[2, i] # a character string describing the parameter space, that will be parsed directly by the model
       }
       else {
-        param <-
-          "" # a zero-sized parameter string will be interpreted by the model as "deduce the parameter space automatically"
+        param <- "" # a zero-sized parameter string will be interpreted by the model as "deduce the parameter space automatically"
       }
-      if (!descriptor[1, i] %in% names(data)) {
-        # descriptor does not match data
-        warnLog <- paste(
-          warnLog,
-          "Variable ",
-          currId,
-          " asked for in descriptor file ",
-          "but absent from the data file.\n",
-          sep = ""
-        )
+      if (! (currId %in% colnames(data))) { # descriptor does not match data
+        warnLog <- paste(warnLog,
+                         "Variable ", currId, " asked for in descriptor file ",
+                         "but absent from the data file.\n",
+                         sep = "")
       }
       else {
         # perform length check and generate named list for current variable
         res <- addVariable(lm,
-                           data[[currId]],
+                           data[,currId],
                            currId, # id
                            currModel, # model
                            param)
-        lm <-
-          res$lm # this discards the warnLog output by addVariable
+        lm <- res$lm # this discards the warnLog output by addVariable
         warnLog <- paste(warnLog,
                          res$warnLog,
                          sep = "")
