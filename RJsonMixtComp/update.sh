515fc3dd4047e174714a3d28ff9fd600ab9c6cfb
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-10-11 14:18:51 +0200

    change update.sh

--- RJsonMixtComp/update.sh
+++ RJsonMixtComp/update.sh
@@ -1,6 +1,7 @@
 cp ../RMixtComp/R/MIXTCOMP_getter.R ./R/MIXTCOMP_getter.R
+cp ../RMixtComp/R/MIXTCOMP_getParam.R ./R/MIXTCOMP_getParam.R
 cp ../RMixtComp/R/MIXTCOMP_misc.R ./R/MIXTCOMP_misc.R
 cd ../MixtComp/
 ./clean.sh
-./buildDebug.sh
-cp debug/json/jmc ../RJsonMixtComp/inst/exeMixtComp/jmc
+./buildRelease.sh
+cp release/json/jmc ../RJsonMixtComp/inst/exeMixtComp/jmc
