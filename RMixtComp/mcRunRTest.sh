d0dd9267f4778f8b1ed267ee09b283a6c4b1fc15
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-10-11 18:27:56 +0200

    load all functions from MixtComp (exported and unexported)

--- RMixtComp/mcRunRTest.sh
+++ RMixtComp/mcRunRTest.sh
@@ -1,3 +1,3 @@
 make cleanRMCbase
 make all
-Rscript -e 'library(methods); library(testthat); library(RMixtComp); options(testthat.output_file = "RMCtest.xml"); test_dir("tests/testthat/", reporter = JunitReporter)'
+Rscript -e 'library(methods); library(testthat); library(devtools); load_all(); options(testthat.output_file = "RMCtest.xml"); test_dir("tests/testthat/", reporter = JunitReporter)'
