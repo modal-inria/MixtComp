19b8e56548c90feedcdb6bccf950252c218411ec
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2018-12-14 14:15:43 +0100

    weibull generator manages new type of missing

--- RMixtComp/R/GENDATA_simpleModelGenerator.R
+++ RMixtComp/R/GENDATA_simpleModelGenerator.R
@@ -104,14 +104,16 @@ weibullFullGenerator <- function(param) {
 
 weibullHideData <- function(present, x, param) {
   if(!present) {
-    missType <- sample(2, size = 1)
+    missType <- sample(4, size = 1)
     
     bounds <- round(sort(rweibull(2, param$shape, param$scale)), 5)
     
     x <- switch(
       missType,
       "1" = "?",
-      "2" = paste0("[",bounds[1],":","+inf","]"))
+      "2" = paste0("[",bounds[1],":","+inf","]"),
+      "3" = paste0("[","-inf",":",bounds[2],"]"),
+      "4" = paste0("[",bounds[1],":",bounds[2],"]"))
   }
   
   return(as.character(x))
