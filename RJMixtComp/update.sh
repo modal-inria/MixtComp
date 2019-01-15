20b2a17253e58774fd196e1aaf65bc748f167c5a
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-01-15 16:31:08 +0100

    update build instructions

--- RJMixtComp/update.sh
+++ RJMixtComp/update.sh
@@ -2,7 +2,8 @@ cp ../RMixtComp/R/MIXTCOMP_getter.R ./R/MIXTCOMP_getter.R
 cp ../RMixtComp/R/MIXTCOMP_getParam.R ./R/MIXTCOMP_getParam.R
 cp ../RMixtComp/R/MIXTCOMP_misc.R ./R/MIXTCOMP_misc.R
 cp ../RMixtComp/R/MIXTCOMP_formatData.R ./R/MIXTCOMP_formatData.R
+cp ../RMixtComp/R/MIXTCOMP_formatFunctions.R ./R/MIXTCOMP_formatFunctions.R
 cd ../MixtComp/
 ./clean.sh
 ./buildRelease.sh
-cp release/json/jmc ../RJsonMixtComp/inst/exeMixtComp/jmc
+cp release/json/jmc ../RJMixtComp/inst/exeMixtComp/jmc
