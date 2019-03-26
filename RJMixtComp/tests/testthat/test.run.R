ca9c5b5d373a53d45b07d51d497c25875bf1e53a
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-03-26 10:59:20 +0100

    test functional data

--- RJMixtComp/tests/testthat/test.run.R
+++ RJMixtComp/tests/testthat/test.run.R
@@ -32,6 +32,37 @@ test_that("Learn + predict", {
   file.remove("./algo.json", "./model.json", "./data.json", "reslearn.json", "respredict.json")
 })
 
+
+
+test_that("Learn + predict functional", {
+  pathToData <- system.file("extdata", "functionalData.RData", package = "RJMixtComp")
+  load(pathToData)
+  
+  expect_silent(resLearn <- JMixtCompLearn(data, model, algo, nClass = 2,
+                                           inputPath = ".", outputFile = "reslearn.json"))
+  
+  expect_true(is.null(resLearn$warnLog))
+  expect_true(file.exists("./algo.json"))
+  expect_true(file.exists("./model.json"))
+  expect_true(file.exists("./data.json"))
+  expect_true(file.exists("reslearn.json"))
+
+  
+  expect_silent(resPredict <- JMixtCompPredict(data, model, algo, nClass = 2, inputPath = ".",
+                                               paramFile = "reslearn.json", outputFile = "respredict.json"))
+  expect_true(is.null(resPredict$warnLog))
+  expect_true(file.exists("./algo.json"))
+  expect_true(file.exists("./model.json"))
+  expect_true(file.exists("./data.json"))
+  expect_true(file.exists("reslearn.json"))
+  expect_true(file.exists("respredict.json"))
+  
+  file.remove("./algo.json", "./model.json", "./data.json", "reslearn.json", "respredict.json")
+  
+})
+
+
+
 test_that("can predict with only one sample in the data set", {
   pathToData <- system.file("extdata", "data.json", package = "RJMixtComp")
   pathToDescriptor <- system.file("extdata", "desc.json", package = "RJMixtComp")
