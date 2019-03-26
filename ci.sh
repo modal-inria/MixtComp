739909626fe230391446a1339a826d0275efa3ba
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-03-26 15:32:14 +0100

    add RJMixtComp tests to ci.sh

--- ci.sh
+++ ci.sh
@@ -4,10 +4,14 @@
 
 cd MixtComp
 ./clean.sh
-./buildDebug.sh
-./valgrindRun.sh
+./buildRelease.sh
+#./buildDebug.sh
+#./valgrindRun.sh
 cd ../RMixtComp
 make clean
 make updateLib
 make all
 Rscript -e 'library(methods); library(devtools); options(testthat.output_file = "RMCtest.xml"); test(".", reporter = JunitReporter)'
+cd ../RJMixtComp
+./update.sh
+Rscript -e 'library(methods); library(devtools); options(testthat.output_file = "RJMCtest.xml"); test(".", reporter = JunitReporter)'
