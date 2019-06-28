8dbb0ac26980a174ab9b3f2eaa30d33f65c2ee92
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-06-28 17:14:13 +0200

    add file

---
+++ pyMixtComp/src/pythonIO.h
@@ -0,0 +1,17 @@
+/* MixtComp version 2.0  - 13 march 2017
+ * Copyright (C) Inria - Lille 1 */
+
+/*
+ *  Project:    MixtComp
+ *  Created on: Jun 28, 2019
+ *  Authors:    Quentin GRIMONPREZ <quentin.grimonprez@inria.fr>
+ **/
+
+#ifndef PYTHON_PYTHONIO_H
+#define PYTHON_PYTHONIO_H
+
+#include "PyGraph.h"
+#include "translateCPPToPython.h"
+#include "translatePythonToCPP.h"
+
+#endif
