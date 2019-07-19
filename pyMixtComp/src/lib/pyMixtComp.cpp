c705ca645ca1671577760a30f896b4f9acf8917e
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-07-19 16:32:24 +0200

    intialize numpy

--- pyMixtComp/src/lib/pyMixtComp.cpp
+++ pyMixtComp/src/lib/pyMixtComp.cpp
@@ -1,10 +1,12 @@
 #include <boost/python.hpp>
-
+#include <boost/python/numpy.hpp>
 #include "pmc.h"
 
 
 
 BOOST_PYTHON_MODULE(pyMixtComp)
 {
+	Py_Initialize();
+	boost::python::numpy::initialize();
 	boost::python::def("pmc", pmc);
 }
