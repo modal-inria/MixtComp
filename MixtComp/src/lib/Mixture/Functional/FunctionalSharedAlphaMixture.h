c32e03dd2ea7a937f918f569a674847d5591cbc5
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-07-22 11:25:11 +0200

    add AGPL licence in C++ files

--- MixtComp/src/lib/Mixture/Functional/FunctionalSharedAlphaMixture.h
+++ MixtComp/src/lib/Mixture/Functional/FunctionalSharedAlphaMixture.h
@@ -1,6 +1,19 @@
 /* MixtComp version 4  - july 2019
  * Copyright (C) Inria - Université de Lille - CNRS*/
 
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ **/
+
 /*
  *  Project:    MixtComp
  *  Created on: June 20, 2016
