7a499d4299e760d8c72749d9d81a9c3d08c740f5
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-07-31 09:27:56 +0200

    add licence & copyrgiht info

--- pyMixtComp/src/lib/pmc.h
+++ pyMixtComp/src/lib/pmc.h
@@ -1,9 +1,24 @@
-/*
- * pmc.h
+/* MixtComp version 4  - july 2019
+ * Copyright (C) Inria - Université de Lille - CNRS*/
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *  Created on: 13 juin 2019
- *      Author: Quentin Grimonprez
- */
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ **/
+
+/*
+ *  Project:    MixtComp
+ *  Created on: June 13, 2019
+ *  Author:    Quentin Grimonprez
+ **/
 
 #ifndef SRC_PMC_H_
 #define SRC_PMC_H_
--- pyMixtComp/src/lib/translatePythonToCPP.h
+++ pyMixtComp/src/lib/translatePythonToCPP.h
@@ -1,10 +1,24 @@
-//
-//  translatePythonToCPP.hpp
-//  MixtComp
-//
-//  Created by Leo Perard on 09/01/2019.
-//
-//
+/* MixtComp version 4  - july 2019
+ * Copyright (C) Inria - Université de Lille - CNRS*/
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ **/
+
+/*
+ *  Project:    MixtComp
+ *  Created on: June, 2019
+ *  Authors:    Quentin Grimonprez
+ **/
 
 #ifndef PYMIXTCOMP_SRC_TRANSLATEPYTHONTOCPP_H
 #define PYMIXTCOMP_SRC_TRANSLATEPYTHONTOCPP_H
