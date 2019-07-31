a47d53b4be9159240bf29890ec1f72cd3e0bd4cb
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-07-31 14:19:15 +0200

    update test compilation script

--- JMixtComp/utestDebug.sh
+++ JMixtComp/utestDebug.sh
@@ -1,1 +1,2 @@
+cd debug/ && make runUtestJMC && cd ..
 MC_DETERMINISTIC=T debug/utest/runUtestJMC --gtest_output=xml:reportJMC.xml
--- JMixtComp/utestRelease.sh
+++ JMixtComp/utestRelease.sh
@@ -1,1 +1,2 @@
+cd release/ && make runUtestJMC && cd ..
 MC_DETERMINISTIC=T release/utest/runUtestJMC --gtest_output=xml:reportJMC.xml
