e426788757e3da55535a1575cadd2f2c10172c76
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-08-01 08:55:20 +0200

    update script valgrind

--- JMixtComp/valgrindRun.sh
+++ JMixtComp/valgrindRun.sh
@@ -1,1 +1,1 @@
-MC_DETERMINISTIC=T valgrind --xml=yes --xml-file=valgrindReport.xml debug/utest/runUtestJMC --gtest_output=xml:uTestReport.xml
+MC_DETERMINISTIC=T valgrind --xml=yes --xml-file=valgrindReportJMC.xml debug/utest/runUtestJMC --gtest_output=xml:uTestReportJMC.xml
