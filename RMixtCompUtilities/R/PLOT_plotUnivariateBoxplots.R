75cfb16a49838fda1df7682d82be217b48aadd17
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2019-10-07 16:25:45 +0200

    plotdataboxplot now in ggplot2

--- RMixtCompUtilities/R/PLOT_plotUnivariateBoxplots.R
+++ RMixtCompUtilities/R/PLOT_plotUnivariateBoxplots.R
@@ -15,11 +15,14 @@
  
 
 #' Boxplot per class
-#' 
+#'
+#' Display a boxplot (5%-25%-50%-75%-95% quantile) for the given variable for all classes
+#'   
 #' @param output object returned by \emph{mixtCompLearn} function from \emph{RMixtComp} or \emph{rmcMultiRun} function from \emph{RMixtCompIO}
 #' @param var name of the variable
 #' @param grl if TRUE plot the general distribution of the data
 #' @param ... other parameters (see \emph{Details})
+#'
 #' 
 #' @details For functional data, three other parameters are available:
 #' \describe{
@@ -71,7 +74,7 @@ plotDataBoxplot <- function(output, var, grl = TRUE, ...)
   type <- ifelse(type %in% c("Gaussian", "Weibull", "Poisson", "NegativeBinomial"), "Numerical", type)
   
   switch(type,
-         "Numerical" = plotBoxplotperClass(extractBoundsBoxplotNumericalVble(var, output, class = 1:output$algo$nClass, grl = grl), main = var),
+         "Numerical" = plotBoxplotperClass(extractBoundsBoxplotNumericalVble(var, output, class = 1:output$algo$nClass, grl = grl), var),
          "Multinomial" = plotCategoricalData(extractBoundsBoxplotCategoricalVble(var, output, class = 1:output$algo$nClass, grl = grl), var, class = 1:output$algo$nClass, grl),
          "Func_CS" = plotFunctionalData(output, var, ...),
          "Func_SharedAlpha_CS" = plotFunctionalData(output, var, ...),
@@ -79,31 +82,38 @@ plotDataBoxplot <- function(output, var, grl = TRUE, ...)
 }
 
 # here bounds is a matrix
-# @author Matthieu Marbac
-plotBoxplotperClass <- function(bounds, ...){
-  # Définition des paramètres du graphiques
-  plot(NA,
-       xlim = range(as.numeric(bounds)),
-       ylim = c(0.2, nrow(bounds) + .8 ),
-       xlab = "value",
-       ylab = "class",
-       yaxt = "n",
-       lab = c(10, nrow(bounds), 5),
-       ...)
-  ytick <- 1:nrow(bounds)
-  axis(side = 2, at = ytick, labels = rownames(bounds))
-  # Boucle sur les classes
-  for (k in 1:nrow(bounds)){
-    # Lines horizontales
-    lines(c(bounds[k,1], bounds[k,2]), c(k, k) )
-    lines(c(bounds[k,2], bounds[k,4]), (-.3)* c(1, 1) + k )
-    lines(c(bounds[k,2], bounds[k,4]), (.3)* c(1, 1) + k )
-    lines(c(bounds[k,4], bounds[k,5]), c(k, k) )
-    # Lines verticales
-    lines(rep(bounds[k,1], 2), c(-.15,.15) + k)
-    lines(rep(bounds[k,2], 2), c(-.3,.3) + k)
-    lines(rep(bounds[k,3], 2), c(-.3,.3) + k, lwd=2)
-    lines(rep(bounds[k,4], 2), c(-.3,.3) + k)
-    lines(rep(bounds[k,5], 2), c(-.15,.15) + k)
-  }
+# @author Quentin Grimonprez
+plotBoxplotperClass <- function(bounds, var, pkg = c("ggplot2", "plotly"), ...){
+  pkg = match.arg(pkg)
+  
+  p <- switch(pkg, 
+              "ggplot2" = ggplotBoxplotperClass(bounds, var),
+              "plotly" = ggplotBoxplotperClass(bounds, var))
+  
+  p
 }
+
+
+# @author Quentin Grimonprez
+ggplotBoxplotperClass  <- function(bounds, var)
+{
+  labelClass <- factor(rownames(bounds), levels = rownames(bounds))
+  
+  df = as.data.frame(bounds)
+  df$class = labelClass
+  df$classlo = 1:nrow(df) - 0.1
+  df$classup = 1:nrow(df) + 0.1
+  df$classmid = 1:nrow(df) 
+
+  p <- ggplot() +
+    geom_rect(data = df, mapping = aes_string(xmin = "`quantil. 0.25`", xmax = "`quantil. 0.75`", ymin = "classlo", ymax = "classup", fill = "class"), color = "black", alpha = 0.5) + 
+    geom_rect(data = df, mapping = aes_string(xmin = "`quantil. 0.5`", xmax = "`quantil. 0.5`", ymin = "classlo", ymax = "classup"), color = "black")  + 
+    geom_rect(data = df, mapping = aes_string(xmin = "`quantil. 0.05`", xmax = "`quantil. 0.25`", ymin = "classmid", ymax = "classmid"), color = "black")  + 
+    geom_rect(data = df, mapping = aes_string(xmin = "`quantil. 0.75`", xmax = "`quantil. 0.95`", ymin = "classmid", ymax = "classmid"), color = "black") +
+    labs(title = paste0("Boxplot per class for variable ", var), x = var, y = element_blank()) +
+    theme(legend.position = "none") +
+    scale_y_continuous(breaks = 1:nrow(df), labels = df$class)
+  
+  p
+}
+
