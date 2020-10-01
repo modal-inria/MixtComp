b8f31fd85ae64255a305d52b5c585271eacd1016
Quentin Grimonprez <quentingrim@yahoo.fr>
2020-10-01 18:16:58 +0200

    remove test checking if pdf is the derivative of pdf (because we use the methods provided by boost)

--- MixtComp/src/utest/Statistic/UTestWeibullStatistic.cpp
+++ MixtComp/src/utest/Statistic/UTestWeibullStatistic.cpp
@@ -26,7 +26,7 @@
 using namespace mixt;
 
 /**
- * Numerically check that pdf is the derivative of cdf, as per the definition. And that lpdf is correctly computed.
+ * Numerically check that lpdf is correctly computed.
  * */
 TEST(WeibullStatistic, DistributionFunctions) {
 	Real k = 2.3;
@@ -35,13 +35,8 @@ TEST(WeibullStatistic, DistributionFunctions) {
 
 	WeibullStatistic weibull;
 
-	Real cdf1 = weibull.cdf(obs + epsilon, k, lambda);
-	Real cdf0 = weibull.cdf(obs, k, lambda);
-
-	Real derivPDF = (cdf1 - cdf0) / epsilon;
 	Real directPDF = weibull.pdf(obs, k, lambda);
 	Real directLPDF = weibull.lpdf(obs, k, lambda);
 
-	ASSERT_NEAR(derivPDF, directPDF, epsilon);
 	ASSERT_NEAR(std::log(directPDF), directLPDF, epsilon);
 }
