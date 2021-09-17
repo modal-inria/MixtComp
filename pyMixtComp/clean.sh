3eb0af37a193470c907b6d20da0ed593a89a6b73
Quentin Grimonprez <quentingrim@yahoo.fr>
2021-09-17 11:51:16 +0200

    ci: re-add debug and release folder

--- pyMixtComp/clean.sh
+++ pyMixtComp/clean.sh
@@ -1,3 +1,10 @@
+cd debug
+find . ! -name 'empty' -type f -exec rm -f {} +
+find . ! -name '.' -type d -exec rm -r -f {} +
+cd ../release 
+find . ! -name 'empty' -type f -exec rm -f {} +
+find . ! -name '.' -type d -exec rm -r -f {} +
+cd ..
 find ./build ! -name '.' -type f -exec rm -f {} +
 find ./build ! -name '.' -type d -exec rm -r -f {} +
 
