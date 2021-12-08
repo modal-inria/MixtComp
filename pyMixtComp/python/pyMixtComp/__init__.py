83f087c38c205ea6667056376aba3cc0880eb16e
Quentin Grimonprez <qgrimonprez@diagrams-technologies.com>
2021-12-08 18:20:55 +0100

    docs: add version info

--- pyMixtComp/python/pyMixtComp/__init__.py
+++ pyMixtComp/python/pyMixtComp/__init__.py
@@ -3,3 +3,6 @@ from .MixtComp import MixtComp
 from . import data
 from . import utils
 from . import plot
+from pkg_resources import get_distribution
+
+__version__ = get_distribution("pyMixtComp").version
